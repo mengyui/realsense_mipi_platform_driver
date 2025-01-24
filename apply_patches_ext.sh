619007229c39365e6fe1f6d9200d4f48f805236c
Yu Meng <yu1.meng@intel.com>
2025-01-24 18:42:45 +0800

    Enable JP 6.2

--- apply_patches_ext.sh
+++ apply_patches_ext.sh
@@ -3,6 +3,7 @@
 # JP 6.0 DP https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v2.0/sources/public_sources.tbz2
 # JP 6.0 https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v3.0/sources/public_sources.tbz2
 # JP 6.1 https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v4.0/sources/public_sources.tbz2
+# JP 6.2 https://developer.nvidia.com/downloads/embedded/l4t/r36_release_v4.3/sources/public_sources.tbz2
 
 set -e
 
@@ -34,13 +35,17 @@ cd "$DEVDIR"
 if [[ "$JETPACK_VERSION" == "4.6.1" ]]; then
     JP5_D4XX_DTSI="tegra194-camera-d4xx.dtsi"
 fi
-if [[ "$JETPACK_VERSION" == "6.0" ]]; then
-    D4XX_SRC_DST=nvidia-oot
-elif [[ "$JETPACK_VERSION" == "6.1" ]]; then
+
+if [[ $JETPACK_VERSION == 4* ]]; then
+    D4XX_SRC_DST=kernel/nvidia
+elif [[ $JETPACK_VERSION == 5* ]]; then
+    D4XX_SRC_DST=kernel/nvidia
+elif [[ $JETPACK_VERSION == 6* ]]; then
     D4XX_SRC_DST=nvidia-oot
 else
-    D4XX_SRC_DST=kernel/nvidia
+    D4XX_SRC_DST=nvidia-oot
 fi
+
 # NVIDIA SDK Manager's JetPack 4.6.1 source_sync.sh doesn't set the right folder name, it mismatches with the direct tar
 # package source code. Correct the folder name.
 if [ -d $1/hardware/nvidia/platform/t19x/galen-industrial-dts ]; then
@@ -55,27 +60,24 @@ apply_external_patches $1 $D4XX_SRC_DST
 if [ -d ${KERNEL_DIR}/${JETPACK_VERSION} ]; then
     apply_external_patches $1 $KERNEL_DIR
 fi
-if [[ "$JETPACK_VERSION" == "6.0" ]]; then
-    apply_external_patches $1 hardware/nvidia/t23x/nv-public
-elif [[ "$JETPACK_VERSION" == "6.1" ]]; then
-    apply_external_patches $1 hardware/nvidia/t23x/nv-public
-else
+
+if [[ $JETPACK_VERSION == 4* ]]; then
     apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
     apply_external_patches $1 hardware/nvidia/platform/t23x/concord/kernel-dts
     apply_external_patches $1 hardware/nvidia/platform/t23x/p3768/kernel-dts
+elif [[ $JETPACK_VERSION == 5* ]]; then
+    apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
+    apply_external_patches $1 hardware/nvidia/platform/t23x/concord/kernel-dts
+    apply_external_patches $1 hardware/nvidia/platform/t23x/p3768/kernel-dts
+elif [[ $JETPACK_VERSION == 6* ]]; then
+    apply_external_patches $1 hardware/nvidia/t23x/nv-public
+else
+    aapply_external_patches $1 hardware/nvidia/t23x/nv-public
 fi
 
 # For a common driver for JP4 + JP5 we override the i2c driver and ignore the previous that was created from patches
 cp $DEVDIR/kernel/realsense/d4xx.c $DEVDIR/$1/${D4XX_SRC_DST}/drivers/media/i2c/
-if [[ "$JETPACK_VERSION" == "6.0" ]]; then
-    # jp6 overlay
-    cp $DEVDIR/hardware/realsense/tegra234-camera-d4xx-overlay.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-    cp $DEVDIR/hardware/realsense/tegra234-camera-d4xx-overlay-dual.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-    cp $DEVDIR/hardware/realsense/tegra234-p3737-camera-d4xx-overlay-fg96-8ch-dual.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-    cp $DEVDIR/hardware/realsense/tegra234-p3737-camera-d4xx-overlay-fg96-8ch-single.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-    cp $DEVDIR/hardware/realsense/tegra234-p3768-camera-d4xx-overlay-fg96-2ch-dual.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-    cp $DEVDIR/hardware/realsense/tegra234-p3768-camera-d4xx-overlay-fg96-2ch-single.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
-elif [[ "$JETPACK_VERSION" == "6.1" ]]; then
+if [[ $JETPACK_VERSION == 6* ]]; then
     # jp6 overlay
     cp $DEVDIR/hardware/realsense/tegra234-camera-d4xx-overlay.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
     cp $DEVDIR/hardware/realsense/tegra234-camera-d4xx-overlay-dual.dts $DEVDIR/$1/hardware/nvidia/t23x/nv-public/overlay/
